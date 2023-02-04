8ca63a8895b10d2c653575622e8fda50bc71a59e
Brandon Moy <brandonkaghenmoy@gmail.com>
2023-02-04 20:27:47 +0000

    Notes on IF statements from Lesson 3 of Python 4 Everybody

---
+++ Lessons/ch3.py
@@ -0,0 +1,17 @@
+# Conditional Execution
+
+x = 5 # Assign an int to a variable
+if x < 10: # IF statement - no parenthesis and uses : (colon) to go next
+  print('Smaller'); # condition code blocl is indented below the IF statement
+  print('Than 10');
+if x > 20: print('Bigger'); # You can one line if only running 1 line of conditional code
+
+print('Finis');
+
+# Pyathon Comparison Operators
+# < Less Than
+# <= Less than or Equal to
+# == Equal to
+# >= Greater than or Equal to
+# > Greater than
+# != Not equal
