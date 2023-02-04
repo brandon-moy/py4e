9a479cadbf6d461b243cecfce63fddb94f9878a2
Brandon Moy <brandonkaghenmoy@gmail.com>
2023-02-04 18:27:41 +0000

    Wrote code from py4e ch 1 lesson to test syntax and running code

--- first.py
+++ first.py
@@ -3,3 +3,15 @@ x = x + 2;
 print(x);
 
 # use the command python3 file-name.py to run it (like how you would run node)
+
+# if syntax does not use parenthesis and uses colon for code on indented line
+if x < 5:
+  print('Small');
+if x > 5:
+  print('Big');
+
+n = 5
+while n > 0 :
+    print(n);
+    n = n - 1
+print('Blastoff!')
