d932614b521402b6b8d326340b797ebb4e1a0151
Brandon Moy <brandonkaghenmoy@gmail.com>
2023-02-04 20:07:28 +0000

    Completed exercise 0202 for Python 4 Everybody

---
+++ Exercises/0202.py
@@ -0,0 +1,12 @@
+# Write a program that uses input to promts a user for their name and then welcomes them.
+# Enter your name: ______
+# Hello _______
+
+name = input('Enter your name: ');
+print('Hello', name);
+
+# Which line of code will ask a user for a text input and store it to a variable?
+### a) num = input("What is your favorite number? ") ###
+# b) num = cin("What is your favorite number? ")
+# a) num = prompt("What is your favorite number? ")
+# a) num = print("What is your favorite number? ")
